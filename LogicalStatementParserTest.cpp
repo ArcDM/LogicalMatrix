ed2066c266c9ddc2e806b798e6c6133ee564fccf
ArcDM <DarkMerlin21st@gmail.com>
2019-11-05 15:04:07 -0600

    Added printing of unique identifiers

--- LogicalStatementParserTest.cpp
+++ LogicalStatementParserTest.cpp
@@ -5,30 +5,40 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
 #include "LogicalStatementParser.h"
+
+// used to print unique identifiers from LogicalStatementParser
+std::ostream &operator<<( std::ostream &output, const std::set< std::string > &object_arg )
+{
+    for( auto &value : object_arg )
+    {
+        output << "\"" << value << "\" ";
+    }
+
+    return output;
+}
 
 void test( const std::string &tested, const std::string &expected )
 {
-    std::ostringstream stringstream;
     LogicalStatementParser test_parser( tested );
-    stringstream << test_parser;
 
     std::cout << "Testing \"" << tested << "\" and \"" << expected << "\"" << std::endl
-        << "Result: " << test_parser << std::endl << "Test "
-        << ( ( expected.compare( stringstream.str() ) == 0 )? "passed" : "FAILED" ) << std::endl << std::endl;
+        << "Result: " << test_parser << std::endl
+        << "Test "<< ( ( expected.compare( test_parser.to_string() ) == 0 )? "passed" : "FAILED" ) << std::endl
+        << "Unique Identifiers: " << test_parser.get_unique_identifiers() << std::endl // spaced to comment out
+        << std::endl;
 }
 
 int main( int argc, char const *argv[] )
 {
     test( " a & b & c & d", "a & b & c & d" );
-    test( " a & b & c | d ", "a & b & c | d" );
-    test( "a & b | c & d ", "a & b | c & d" );
-    test( "a &  b | c | d", "a & b | c | d" );
-    test( "a  |  b & c & d", "a | b & c & d" );
-    test( "a|b&c|d", "a | b & c | d" );
-    test( "a | b | c & d", "a | b | c & d" );
-    test( "a | b | c | d", "a | b | c | d" );
+    test( " NOT a AND b & c OR d ", "!a & b & c | d" );
+    test( "a & !b | c & d ", "a & !b | c & d" );
+    test( "a &  b | !c | d", "a & b | !c | d" );
+    test( "a  |  b & c & !   d", "a | b & c & !d" );
+    test( "!a|b&c|!d", "!a | b & c | !d" );
+    test( "a | !b | ! !  !c & d", "a | !b | !c & d" );
+    test( "!a | !!b | !!!c | !!!!d", "!a | b | !c | d" );
     /*LogicalStatementParser test1( " a & (b || c) | d" ); // = a & b | a & c | d
 
     LogicalStatementParser test2( "a && (b | c) & d " ); // = a & b & d | a & c & d
