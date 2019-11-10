b7242e0047d9bd279c28b7a7c03f06bbd8576725
ArcDM <DarkMerlin21st@gmail.com>
2019-11-09 21:20:32 -0600

    Improved testing output organization

--- LogicalStatementParserTest.cpp
+++ LogicalStatementParserTest.cpp
@@ -18,41 +18,50 @@ std::ostream &operator<<( std::ostream &output, const std::set< std::string > &o
     return output;
 }
 
-void test( const LogicalStatementParser &test_parser, const std::string &expected )
+bool test( const LogicalStatementParser &test_parser, const std::string &expected )
 {
+    bool result = expected.compare( test_parser.to_string() ) == 0;
+
     std::cout << "Result: " << test_parser << std::endl
-        << "Test "<< ( ( expected.compare( test_parser.to_string() ) == 0 )? "passed" : "FAILED" ) << std::endl
-        << "Unique Identifiers: " << test_parser.get_unique_identifiers() << std::endl // spaced to comment out
-        << std::endl;
+        << "Test "<< ( result? "passed" : "FAILED" ) << std::endl
+        << "Unique Identifiers: " << test_parser.get_unique_identifiers() << std::endl; // spaced to comment out
+
+    return result;
 }
 
-void test( const std::string &tested, const std::string &expected )
+bool test( const std::string &tested, const std::string &expected )
 {
     LogicalStatementParser test_parser( tested );
 
     std::cout << "Testing \"" << tested << "\" and \"" << expected << "\"" << std::endl;
-    test( test_parser, expected );
+    return test( test_parser, expected );
 }
 
 int main( int argc, char const *argv[] )
 {
+    bool result;
+
     if( true )
     {
-        test( " a & b & c & space space", "a & b & c & space space" );
-        test( " NOT a AND b & c OR d ", "!a & b & c | d" );
-        test( "a & !b | c & d ", "a & !b | c & d" );
-        test( "a &  b | !c | space     space", "a & b | !c | space     space" );
-        test( "a  |  b & c & !   d", "a | b & c & !d" );
-        test( "!a|b&c|!d", "!a | b & c | !d" );
-        test( "a | !b | ! !  !c & d", "a | !b | !c & d" );
-        test( "!a | !!b | !!!c | !!!!d", "!a | b | !c | d" );
+        result = test( " a & b & c & space space", "a & b & c & space space" );
+        result &= test( " NOT a AND b & c OR d ", "!a & b & c | d" );
+        result &= test( "a & !b | c & d ", "a & !b | c & d" );
+        result &= test( "a &  b | !c | space     space", "a & b | !c | space     space" );
+        result &= test( "a  |  b & c & !   d", "a | b & c & !d" );
+        result &= test( "!a|b&c|!d", "!a | b & c | !d" );
+        result &= test( "a | !b | ! !  !c & d", "a | !b | !c & d" );
+        result &= test( "!a | !!b | !!!c | !!!!d", "!a | b | !c | d" );
+
+        std::cout << std::endl << "Tests "<< ( result? "passed" : "FAILED" ) << std::endl << std::endl;
     }
 
     if( true )
     {
-        test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "c & d" ), "a & b | c & d" );
-        test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "a & b" ), "a & b" );
-        test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "a" ), "a & b | a" );
+        result = test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "c & d" ), "a & b | c & d" );
+        result &= test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "a & b" ), "a & b" );
+        result &= test( LogicalStatementParser( "a & b" ) | LogicalStatementParser( "a | a & b | a & c" ), "a & b | a | a & c" );
+
+        std::cout << std::endl << "Tests "<< ( result? "passed" : "FAILED" ) << std::endl << std::endl;
     }
 
     /*LogicalStatementParser test1( " a & (b || c) | d" ); // = a & b | a & c | d
